c2d75e123474fb02d78604da72e0fdf383b0fb6b
rahavard01 <136188179+rahavard01@users.noreply.github.com>
2025-07-18 20:08:43 +0330

    Update V2bX.sh

--- V2bX.sh
+++ V2bX.sh
@@ -169,7 +169,7 @@ uninstall() {
     rm /usr/local/V2bX/ -rf
 
     echo ""
-    echo -e "Uninstall successful. If you want to delete this script, run after exiting: ${green}rm /usr/bin/V2bX -f${plain} 进行删除"
+    echo -e "Uninstall successful. If you want to delete this script, run after exiting: ${green}rm /usr/bin/V2bX -f${plain} Delete"
     echo ""
 
     if [[ $# == 0 ]]; then
